8c88338437d1993673c4a33bd3ba966f8676b005
Anita Jiang <107587689+jianganita@users.noreply.github.com>
2025-01-11 17:37:29 -0500

    Add files via upload

--- pg3script.js
+++ pg3script.js
@@ -6,8 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
       const value = slider.value;
 
       // Optionally, change the color based on the slider value
-      const red = Math.min(255, value * 2);
-      const blue = 255 - value * 2;
-      box.style.backgroundColor = `rgb(${red}, 0, ${blue})`;
+      const hue = (value * 3.6);
+      box.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
     });
   });
