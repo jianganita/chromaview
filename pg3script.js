0fcd6c8d9aa9b3d03dee61b79a2bc82e62486252
Jason <130693366+jasonpengg@users.noreply.github.com>
2025-01-11 22:29:33 -0500

    pg3 update

--- pg3script.js
+++ pg3script.js
@@ -1,13 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const slider = document.getElementById('slider');
-    const box = document.getElementById('box2');
-  
-    slider.addEventListener('input', () => {
-      const value = slider.value;
+  const slider = document.getElementById('slider');
+  const box = document.getElementById('box2');
 
-      // Optionally, change the color based on the slider value
-      const red = Math.min(255, value * 2);
-      const blue = 255 - value * 2;
-      box.style.backgroundColor = `rgb(${red}, 0, ${blue})`;
-    });
+  slider.addEventListener('input', () => {
+    const value = slider.value;
+
+    // Optionally, change the color based on the slider value
+
+    const hue = (value * 3.6);
+    box.style.backgroundColor = `hsl(${hue}, 100%, 50%)`;
   });
+});
+
+const button = document.getElementById("Submit");
+
+function onClick(){
+  console.log("done");
+  let box1 = document.getElementById('box1');
+  let box2 = document.getElementById('box2');
+  
+  ComputedStyle1 = getComputedStyle(box1);
+  color1 = ComputedStyle1.backgroundColor;
+  rgb1 = color1.match(/\d+/g).map(Number);
+  console.log(rgb1);
+  console.log(rgb1[0]);
+  
+  ComputedStyle2 = getComputedStyle(box2);
+  color2 = ComputedStyle1.backgroundColor;
+  rgb2 = color2.match(/\d+/g).map(Number);
+
+
+  redError = Math.abs((rgb1[0]-rgb2[0]))
+
+  greenError = Math.abs((rgb1[1]-rgb2[1]));
+
+  blueError = Math.abs((rgb1[2]-rgb2[2]));
+
+  tolerance = 30; 
+
+  console.log(redError +","+ greenError +","+ blueError);
+}
+button.addEventListener('click', onClick);
